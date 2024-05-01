b26041224acd4ae670384db992033734d3b0d70b
stephan richard simon <stephansimon123@gmail.com>
2024-05-01 19:06:13 -0300

    Adjusted nomenclatures.

---
+++ prisma/migrations/20240501220242_adjusted_nomenclatures/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Plans` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "clients" DROP CONSTRAINT "clients_planId_fkey";
+
+-- DropTable
+DROP TABLE "Plans";
+
+-- DropTable
+DROP TABLE "Users";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "plans" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "value" INTEGER NOT NULL,
+
+    CONSTRAINT "plans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "clients" ADD CONSTRAINT "clients_planId_fkey" FOREIGN KEY ("planId") REFERENCES "plans"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
